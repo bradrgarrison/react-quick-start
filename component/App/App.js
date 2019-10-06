90047879446eb9a0d282b8e6867d09c46837c6b1
Brad Garrison <bradgarrison@brads-mbp.lan>
2019-10-06 12:29:33 -0700

    committing

---
+++ component/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component {
+    render() {
+        return(
+            <h1 className={styles.Header}>
+                Hello React!!
+            </h1>
+        );
+    }
+}
+
+export default App;
